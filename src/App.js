53351f64aaaf6fef10e81f6ea855ffd5f7d60516
CubiZm <karelense@gmail.com>
2018-08-15 14:43:46 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello, World! I calculate your life.
+      </div>
+    );
+  }
+}
+
+export default App;
